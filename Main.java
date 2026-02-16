2ad84fe911e75cd68a0f4db9a7168cf47ea027a1
Zachary K. <zkesler2027@gmail.com>
2026-02-16 17:06:57 +0000

    Finshed

--- Main.java
+++ Main.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+    String[] answerArray = new String[20];
+    Scanner scan = new Scanner(System.in);
+    for(int i = 0; i < answerArray.length; i ++){
+      System.out.print("Type your answer:");
+      String ans = scan.nextLine();
+      answerArray[i] = ans.toUpperCase();
+    }
+    scan.close();
+    UnitTest stAns = new UnitTest(answerArray);
+    System.out.print(stAns);
+    System.out.println("\nTotal Correct:" + stAns.totalCorrect());
+    System.out.println("Total Mistake:" + stAns.totalMistakes());
+    if(stAns.isPassing()){
+      System.out.println("This student is Passing");
+    }else{
+      System.out.println("This student is Failing");
+    }
     
   }
 }
--- UnitTest.java
+++ UnitTest.java
@@ -1,13 +1,60 @@
 public class UnitTest {
 
   // instance variables
-
-  
+  private String[] answerKey =  {"B", "D", "C", "A", "E", "A", "B", "A", "E", "B", "B", "E", "E", "D", "D", "A", "C", "C", "A", "E"};
+  private String[] studentAns;
   // constructor
+  public UnitTest(String[] arr){
+    studentAns = new String[arr.length];
+    for(int i=0; i < arr.length; i++ ){
+      studentAns[i] = arr[i];
+    }
+  }
 
   
   // instance methods
-
-
+  public String[] getStudentAns(){
+    return studentAns;
+  }
+  public int totalCorrect(){
+    int  total = 0;
+    for(int i=0; i < studentAns.length; i++ ){
+      if(studentAns[i].equals(answerKey[i])){
+        total += 1;
+      }
+      
+    }
+    return total;
+  }
+  public int totalMistakes(){
+    int  total = 0;
+    for(int i=0; i < studentAns.length; i++ ){
+      if(!studentAns[i].equals(answerKey[i])){
+        total += 1;
+      }
+      
+    }
+    return total;
+  }
+  public boolean isPassing(){
+    int  total = 0;
+    for(int i=0; i < studentAns.length; i++ ){
+      if(studentAns[i].equals(answerKey[i])){
+        total ++;
+      }
+      i++;
+    }
+    if (total >= 14){
+      return true;
+    }
+    return false;
+  }
+  public String toString(){
+    String answerTable = "Answer Key\tStudent Ans \n" ;
+    for(int i=0; i < studentAns.length; i++ ){
+      answerTable += (i +1) + ")" + answerKey[i]+"\t"+ (i+ 1)+ ")"+ studentAns[i] +"\n" ;
+    }
+    return answerTable;
+  }
   
 }
